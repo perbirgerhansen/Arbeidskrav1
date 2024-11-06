106806a40bf5abcfc54441599c200a029fe105c9
perbirgerhansen <perbirgerhansen@outlook.com>
2024-11-06 15:42:34 +0100

    Update Arbeidskrav1_v01.py
    Endret tekst på avgift

--- Arbeidskrav1_v01.py
+++ Arbeidskrav1_v01.py
@@ -14,7 +14,7 @@ Created on Tue Nov 06 18:34:45 2024
 KM = 10000 #Antall km Pr. År
 Forsikring_EL = 5000 #Forsikring for elbil pr. år 
 Forsikring_Bensin = 7500 #Forsikring for bensinbil pr. år
-Trafikkavg = 8.38*365 #Trafikkforsikringsavgift pr. dag både for El og Bensin 
+Trafikkavg = 8.38*365 #Trafikkforsikringsavgift år både for El og Bensin 
 Strombruk_EL = 0.2 #Strømbruk pr. Kwh 
 Strompris = 2.00*Strombruk_EL*KM #Strømpris * bruk * KM Elbil 
 Bensinpris = 1.0*KM #Bensinpris * km bensinbil
